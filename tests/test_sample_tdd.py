08ac1d1d525574f0579651a71adbd40415188e9c
Satoshi Yamagami <s.yamagami.666@gmail.com>
2020-03-04 19:04:27 +0900

    test:テストサンプルの追加

--- tests/test_sample_tdd.py
+++ tests/test_sample_tdd.py
@@ -1,6 +1,9 @@
-import pytest
-from sample_tdd import __version__
+from sample_tdd import __version__, __all__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_all():
+    assert __all__ == ["hello_world"]
